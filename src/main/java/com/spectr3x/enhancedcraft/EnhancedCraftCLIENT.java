c36b5beae72ca0782de3bd281b803657efcd4bc3
PheonixVX <TheGrimReaper.VX@gmail.com>
2020-10-03 13:22:36 -0400

    Add haunted entity partially...

--- src/main/java/com/spectr3x/enhancedcraft/EnhancedCraftCLIENT.java
+++ src/main/java/com/spectr3x/enhancedcraft/EnhancedCraftCLIENT.java
@@ -1,13 +1,19 @@
 package com.spectr3x.enhancedcraft;
 
+import com.spectr3x.enhancedcraft.entity.HeroBrineEntityRenderer;
 import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
 import me.sargunvohra.mcmods.autoconfig1u.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry;
 
 public class EnhancedCraftCLIENT implements ClientModInitializer {
 
 	@Override
 	public void onInitializeClient() {
+		// Entity Registration
+		EntityRendererRegistry.INSTANCE.register(EnhancedCraft.HEROBRINE, (dispatcher, context) -> {
+			return new HeroBrineEntityRenderer(dispatcher);
+		});
 		// AutoConfig1u Registry
 		AutoConfig.register(ModConfig.class, GsonConfigSerializer::new);
 	}
--- src/main/java/com/spectr3x/enhancedcraft/mixin/PlayerSleepMixin.java
+++ src/main/java/com/spectr3x/enhancedcraft/mixin/PlayerSleepMixin.java
@@ -2,6 +2,7 @@ package com.spectr3x.enhancedcraft.mixin;
 
 import com.mojang.datafixers.util.Either;
 import com.spectr3x.enhancedcraft.EnhancedCraft;
+import com.spectr3x.enhancedcraft.entity.HeroBrineEntity;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
@@ -10,6 +11,8 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Unit;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -21,10 +24,10 @@ import java.util.Random;
 public class PlayerSleepMixin {
 	PlayerEntity player = (PlayerEntity) (Object) this;
 
-	@Inject(method = "trySleep(Lnet/minecraft/util/math/BlockPos;)Lcom/mojang/datafixers/util/Either;", at = @At(value = "HEAD"))
+	@Inject(method = "trySleep(Lnet/minecraft/util/math/BlockPos;)Lcom/mojang/datafixers/util/Either;", at = @At(value = "HEAD"), cancellable = true)
 	private void trySleep (BlockPos pos, CallbackInfoReturnable<Either<PlayerEntity.SleepFailureReason, Unit>> cir) {
 		Random random = new Random();
-		int i = random.nextInt(100);
+		int i = random.nextInt(2);
 		if (player.world.isClient()) {
 			ClientPlayerEntity ClientPlayer = (ClientPlayerEntity) player;
 			EnhancedCraft.logger.info("Client Player - Playing Sound");
@@ -32,15 +35,34 @@ public class PlayerSleepMixin {
 			if (i == 1) {
 				ClientPlayer.addStatusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 10 * 20));
 				ClientPlayer.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 10 * 20));
+				ClientPlayer.addStatusEffect(new StatusEffectInstance(StatusEffects.WEAKNESS, 10 * 20));
+
+				// Spawm Haunted Herobrine
+				HeroBrineEntity herobrine = new HeroBrineEntity(EnhancedCraft.HEROBRINE, ClientPlayer.world);
+				ClientPlayer.world.spawnEntity(herobrine);
+				BlockPos playerPos = new BlockPos(ClientPlayer.getPos().add(ClientPlayer.getRotationVector().multiply(1)));
+				herobrine.setPos(playerPos.getX(), playerPos.getY(), playerPos.getZ());
+
 				cir.cancel();
 			}
 		} else {
 			ServerPlayerEntity ServerPlayer = (ServerPlayerEntity) player;
 			EnhancedCraft.logger.info("Server Player - Playing Sound");
 			ServerPlayer.playSound(SoundEvents.ITEM_CROSSBOW_SHOOT, 1.0f, 1.0f);
+			System.out.println(ServerPlayer);
 			if (i == 1) {
 				ServerPlayer.addStatusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 10 * 20));
 				ServerPlayer.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 10 * 20));
+				ServerPlayer.addStatusEffect(new StatusEffectInstance(StatusEffects.WEAKNESS, 10 * 20));
+
+				// Spawm Haunted Herobrine
+				HeroBrineEntity herobrine = new HeroBrineEntity(EnhancedCraft.HEROBRINE, ServerPlayer.world);
+				BlockPos playerPos = new BlockPos(ServerPlayer.getPos().add(ServerPlayer.getRotationVector().multiply(1)));
+				System.out.println(playerPos);
+				herobrine.setPos(playerPos.getX(), playerPos.getY(), playerPos.getZ());
+				ServerPlayer.world.spawnEntity(herobrine);
+				System.out.println(herobrine);
+
 				cir.cancel();
 			}
 		}
